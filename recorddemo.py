11c6e4e62a533e60e8a44965632831adff57c513
anaximeno <anaximenobrito@gmail.com>
2021-09-08 23:47:12 -0100

    updates

--- recorddemo.py
+++ recorddemo.py
@@ -1,32 +1,55 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+tqdm_available: bool = False
+
+try:
+    from tqdm import tqdm
+    tqdm_available = True
+except ImportError:
+    pass
 
 import pyaudio
 import wave
-
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+import os
+from time import sleep
 
 
-# Using the `with` method to open file, which is more secure
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-print("* recording")
-frames = [
-  stream.read(CHUNK) for i in range(0, int(RATE / CHUNK * RECORD_SECONDS))
-]
-print("* done recording")
-stream.stop_stream()
-p.terminate()
+def record(seconds: int, format, channels: int, rate: int, chunk: int, *, save: bool = True, name: str = 'output.wav') -> None:
+    """Records the audio and save it inside the file named `name` if `save` is set to True.
+    """
+    p = pyaudio.PyAudio()
+    stream = p.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=chunk)
+
+    secs = round(rate / chunk * seconds)
+    if tqdm_available is True:
+        loop = tqdm(range(0, secs), desc='Recording', ncols=60, bar_format='{desc} ({remaining_s})s: |-{bar}-|')
+    else:
+        print(" Recording ...")
+        loop = range(0, secs)
+
+    os.system('clear')
+    sleep(0.1)
+    frames = [stream.read(chunk) for _ in loop]
+    stream.stop_stream()
+    p.terminate()
+
+    if tqdm_available is False:
+        print(" Done Recording!")
+
+    if save is True:
+        with wave.open(name, 'wb') as wf:
+          wf.setnchannels(channels)
+          wf.setsampwidth(p.get_sample_size(format))
+          wf.setframerate(rate)
+          wf.writeframes(b''.join(frames))
 
 
-# Using the `with` method to open file, which is more secure
-with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
-  wf.setnchannels(CHANNELS)
-  wf.setsampwidth(p.get_sample_size(FORMAT))
-  wf.setframerate(RATE)
-  wf.writeframes(b''.join(frames))
+if __name__ == '__main__':
+    CHUNK: int = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS: int = 2
+    RATE: int = 44100
+    RECORD_SECONDS: int = 5
+    WAVE_OUTPUT_FILENAME: str = "output.wav"
 
+    record(RECORD_SECONDS, FORMAT, CHANNELS, RATE, CHUNK, save=True, name='output.wav')
